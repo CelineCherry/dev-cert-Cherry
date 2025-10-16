a482f1df6d6e7445f2bd216b9d8e644ca060db56
CelineCherry <celinecherryan@gmail.com>
2025-10-16 17:38:07 -0400

    add js file for project with code stubs

---
+++ Girls++.js
@@ -0,0 +1,36 @@
+var taskList = [];
+
+class Task {
+    constructor(){
+
+    }
+}
+
+
+function onAddTask(){
+    //take data and add an object to taskList    
+    sortTaskList()
+}
+
+function sortTaskList(){
+    //sort tasks in list
+    saveTaskList()
+    showTasks()
+}
+
+function saveTaskList(){ // fatemeh
+
+}
+
+function loadTaskList(){
+
+}
+
+function showTasks(){ //fatemeh
+    for(task in taskList){
+        
+    } 
+}
+
+loadTaskList();
+sortTaskList();
