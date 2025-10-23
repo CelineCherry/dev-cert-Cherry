0899c970936c8ea584e7a97f6403e53bd44f45a2
CelineCherry <celinecherryan@gmail.com>
2025-10-22 22:54:07 -0400

    pushed my own changes so far

--- Girls++.js
+++ Girls++.js
@@ -1,4 +1,14 @@
 var taskList = [];
+/*
+Celine:
+Add tasks to a list once the "add task" button is pressed
+Save tasks to localStorage (happens by a callback once Fatemeh function 1 is finished)
+Load tasks from localStorage (happens on page being opened)
+Fatemeh:
+Sort the tasks in the tasklist when "add task" is pressed (via a function called inside Celine function 1)
+show tasks on screen by making copies of a task element (happens after Fatemeh function 1)
+*/
+
 
 class Task {
     constructor(name, due_date, duration, priority, desc, color){
@@ -10,8 +20,17 @@ class Task {
 
 
 function onAddTask(){
-    //take data and add an object to taskList    
-    sortTaskList()
+    alert("exscript");
+    //take data and add an object to taskList
+    var name = document.getElementById("task_title").value;
+    var due_date = document.getElementById("due_date").value;
+    var duration = document.getElementById("time").value;
+    var priority = document.getElementById("priority_select").value;
+    var desc = document.getElementById("task_desc").value;
+    var color = document.getElementById("color").value;
+    alert("exscript");
+    alert([name, due_date, duration, priority, desc, color]);
+    sortTaskList();
 }
 
 function sortTaskList(){
@@ -20,13 +39,19 @@ function sortTaskList(){
     showTasks()
 }
 
+function loadTaskList(){
+
+
+    
+}
+
+
+
 function saveTaskList(){ // fatemeh
 
 }
 
-function loadTaskList(){
 
-}
 
 function showTasks(){ //fatemeh
     for(task in taskList){
